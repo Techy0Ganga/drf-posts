a40971b26de524f47995dc84eef9cf05b8ac3855
Techy0Ganga <aswingangaofficial@gmail.com>
2025-05-12 17:59:34 +0530

    bug log

--- Simple/settings.py
+++ Simple/settings.py
@@ -25,6 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+print(os.getenv('SECRET_KEY'))
+print(os.getenv('DATABASE_URL'))
+
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
