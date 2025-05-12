0fd3f097e79e5824a718c218cf76bedd31c9554d
Techy0Ganga <aswingangaofficial@gmail.com>
2025-05-12 12:38:01 +0530

    settings update

--- Simple/settings.py
+++ Simple/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Simple.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('KEY'))
 }
 
 
